b7edca44b4bba064d54714921287a9a38176943a
cebms <cemendes@gmail.com>
2022-08-03 19:51:19 -0300

    keystroke reading ok

--- kernel.asm
+++ kernel.asm
@@ -1,44 +1,11 @@
 org 0x7c00  
 jmp _main
 
-; _data:
-playerPosition dw 0
+_data:
+    playerPositionY dw 0
+    playerPositionX dw 0
 ; %1 == coluna (x)
 ; %2 == linha (y)
-%macro drawRacket 2
-    
-    mov ah, 0ch ; coloca no modo de pintar pixels
-    mov dx, %2
-
-    for1:
-        mov bx, 32 ;altura da raquete 
-        add bx, %2
-        cmp dx, bx  ; fim da raquete (linhas)
-        je .fim1
-        mov cx, %1  ; inicio da raquete (coluna)
-        jmp .for2
-        jmp for1
-
-        .fim1:
-        jmp sla
-
-        ; laco for para varrer as 8 colunas da imagem
-        .for2:
-        mov bx, 8 ;largura da raquete
-        add bx, %1
-        cmp cx, bx   ; direita da raquete
-        je .fim2
-        mov al, 0x0f
-        mov ah, 0ch
-        int 10h
-        inc cx
-        jmp .for2
-
-        .fim2:
-        inc dx
-        jmp for1
-
-%endmacro
 
 
 _main:
@@ -51,14 +18,17 @@ _main:
     call setVideoMode
 
     ;posicao inicial da raquete
-    mov word[playerPosition], 10
+    mov word[playerPositionY], 10
+    mov word[playerPositionX], 10
 
     mainLoop:
 
-        drawRacket 10, word[playerPosition]
+        call drawRacket
         
-        sla:
         mov ah, 0x01
+        int 16h
+        jz mainLoop
+        mov ah, 00h
         int 16h
         cmp al, 115
         je moveDown
@@ -76,12 +46,47 @@ setVideoMode:
 ret
 
 moveDown:
-    mov ax, word[playerPosition]
+    mov ax, word[playerPositionY]
     inc ax
-    mov [playerPosition], ax
+    mov [playerPositionY], ax
     mov ah, 0x04
     int 16h
 jmp mainLoop
 
+drawRacket:
+    mov ah, 0ch ; coloca no modo de pintar pixels
+    mov dx, word[playerPositionY]
+
+    for1:
+        mov bx, 32 ;altura da raquete 
+        add bx, word[playerPositionY]
+        cmp dx, bx  ; fim da raquete (linhas)
+        je .fim1
+        mov cx, word[playerPositionX]  ; inicio da raquete (coluna)
+        jmp .for2
+        jmp for1
+
+        .fim1:
+        ret
+
+        ; laco for para varrer as 8 colunas da imagem
+        .for2:
+        mov bx, 8 ;largura da raquete
+        add bx, word[playerPositionX]
+        cmp cx, bx   ; direita da raquete
+        je .fim2
+        mov al, 0x0f
+        mov ah, 0ch
+        int 10h
+        inc cx
+        jmp .for2
+
+        .fim2:
+        inc dx
+        jmp for1
+ret
+
 times 510-($-$$) db 0
 dw 0xaa55
+
+
