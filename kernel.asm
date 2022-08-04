ee2766846eb728af8ec9b033fe03abfc4cd1901b
cebms <cemendes@gmail.com>
2022-08-04 15:23:28 -0300

    try to implement new movement mechanic

--- kernel.asm
+++ kernel.asm
@@ -1,3 +1,4 @@
+BITS 16
 org 0x7c00  
 jmp _main
 
@@ -12,6 +13,7 @@ _main:
     mov cx, ax
     mov dx, ax
 
+    call init 
     call setVideoMode
 
     ;posicao inicial da raquete
@@ -20,17 +22,25 @@ _main:
 
     mainLoop:
 
-        call drawRacket
+        ; call drawRacket
         
-        mov ah, 0x01
-        int 16h
+        ; mov ah, 0x01
+        ; int 16h
+        ; jz mainLoop
+        ; mov ah, 00h
+        ; int 16h
+        ;Check if a is pressed 
+        mov al, 1eh           ;a
+        call is_scancode_pressed
         jz mainLoop
-        mov ah, 00h
-        int 16h
-        cmp al, 115 ; key 's'
-        je moveDown
-        cmp al, 119 ; key 'w'
-        je moveUp
+
+        call drawRacket
+
+        ; ;Check if 'd' is pressed 
+        ; mov al, 20h           ;d 
+        ; call is_scancode_pressed
+        ; jz mainLoop
+
         
     jmp mainLoop
 
@@ -125,6 +135,90 @@ drawRacket:
         inc dx
         jmp for1
 ret
+
+init:
+  push ax  
+
+  mov ax, cs
+  mov WORD [old_isr_15 + 02h], ax        
+  ;old_isr_15 is now a far pointer to new_isr_15
+
+  call swap_isr_15          ;Swap the current isr15 with the one in old_isr_15
+
+  pop ax 
+ret
+
+dispose:
+  call swap_isr_15          ;Swap the current isr15 with the one in old_isr_15                  
+
+ret
+
+is_scancode_pressed:
+  push bx
+
+  movzx bx, al 
+  cmp BYTE [scancode_status + bx], 0 
+
+  pop bx 
+ret
+
+swap_isr_15:
+  push eax
+  push es 
+
+  xor ax, ax 
+  mov es, ax 
+
+  cli
+
+  mov eax, DWORD [es: 15h*4]
+  xchg eax, DWORD [old_isr_15]
+  mov DWORD [es: 15h*4], eax 
+
+  sti
+
+  pop es 
+  pop eax
+
+ret
+
+new_isr_15:
+ ;Check for right function
+ cmp ah, 4fh
+ jne _ni15_legacy
+
+ ;Save used regs
+ push bx
+ push ax
+
+
+ movzx bx, al            ;BX = scancode 
+ and bl, 7fh             ;BX = scancode value
+
+ sar al, 07h             ;AL = 0ffh if scancode has bit7 set (release), 00h otherwise
+ not al                  ;AL = 00h if scancode has bit7 set (release), 0ffh otherwise
+
+ ;Save the scancode status
+ mov BYTE [cs:bx + scancode_status], al 
+ ;Increment the count
+ inc WORD [cs:new_scancode]
+
+ pop ax
+ pop bx 
+
+ _ni15_legacy:   
+    ;This is a far jump, in NASM is simply jmp FAR [cs:old_isr_15]
+    ;Ended up this way for debug
+    push WORD [cs: old_isr_15 + 02h] 
+    push WORD [cs: old_isr_15] 
+ retf
+
+
+old_isr_15 dw new_isr_15, 0  
+;Scan code status table
+scancode_status times 128 db 0
+;Scan code count 
+new_scancode dw 0
 
 times 510-($-$$) db 0
 dw 0xaa55
